8283d684039955b9b3ef4b2202cf89bd691550fb
Sujay V <sujaywebdev@gmail.com>
2022-12-19 16:03:42 +0530

    new ENV file & new user Schema , new register route.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const { PORT } = process.env;
 
-app.listen(4000, () => {
-  console.log(`Server is running at PORT 4000...`);
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}...`);
 });
