d63ee50f300fe0854c767e8a45f8dd9bf5150da8
Sujay V <sujaywebdev@gmail.com>
2022-12-19 17:00:28 +0530

    URL in ENV , DB Connection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("./config/database").connect();
 const express = require("express");
 
 const User = require("./model/user");
@@ -10,14 +11,14 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello from Auth System - WEB 3 </h1>");
 });
 
-app.get("/register", (req, res) => {
+app.get("/register", async (req, res) => {
   const { firstname, lastname, email, passowrd } = req.body;
 
   if (!(email && passowrd && firstname && lastname)) {
     res.status(400).send("All fields are required");
   }
 
-  const existingUser = User.findOne({ email });
+  const existingUser = await User.findOne({ email }); //PROMISE
 
   if (existingUser) {
     res.status(401).send("User Already Exists");
